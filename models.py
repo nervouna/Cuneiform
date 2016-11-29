4c072604ac25c5e941e05d822d74182b940990bc
Guan Xiaoyu <guanxy@me.com>
2016-11-29 23:54:30 +0800

    chore: first slice of flesh - templates, static files & models

---
+++ models.py
@@ -0,0 +1,12 @@
+from leancloud import Object
+from leancloud import LeanCloudError
+from leancloud import File
+
+class Post(Object):
+    pass
+
+class Attachment(File):
+    pass
+
+class User(Object):
+    pass
