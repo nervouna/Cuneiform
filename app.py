eaac24c87783c4b241a56a217ee287b82664cad1
Guan Xiaoyu <guanxy@me.com>
2016-11-30 00:43:06 +0800

    feat(models): get post list, get single post & create a new post.

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 # coding: utf-8
 
 from flask import Flask
+from flask import render_template
+from flask import request
+from flask import redirect
+from flask import url_for
 
 # Required for LeanEngine
 from leancloud import Engine
 from leancloud import LeanEngineError
+
+from models import Post
 
 
 app = Flask(__name__)
@@ -14,4 +20,14 @@ engine = Engine(app)
 
 @app.route('/')
 def index():
-    return "There you have it, INDEX!"
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def new_post():
+    post_title = request.form['title']
+    post_content = request.form['content']
+    post = Post()
+    post.set('title', post_title)
+    post.set('content', post_content)
+    post.save()
+    return redirect(url_for('index'))
--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from leancloud import Object
-from leancloud import LeanCloudError
 from leancloud import File
+from leancloud import Query
+from leancloud import LeanCloudError
 
 class Post(Object):
     pass
@@ -10,3 +11,41 @@ class Attachment(File):
 
 class User(Object):
     pass
+
+def get_post_list(limit=10, page=1):
+    '''Return the post list.
+
+    Keyword arguments:
+    limit -- posts per page (default 10)
+    page -- page number (default 1)
+    '''
+    post_query = Query(Post)
+    post_query.limit(limit)
+    if page > 1: post_query.skip((page - 1) * limit)
+    post_query.add_descending('createdAt')
+    post_list = post_query.find()
+    return post_list
+
+def get_single_post(post_id):
+    '''Return a single post.
+
+    Argument:
+    post_id -- LeanCloud Object ID (default None)
+    '''
+    post_query = Query(Post)
+    single_post = post_query.get(post_id)
+    return single_post
+
+def create_new_post(title, content):
+    '''Create a new post, return ``post_id`` for the created post.
+
+    Argument:
+    title -- title for the new post
+    content -- content for the new post, multi-line text
+    '''
+    new_post = Post()
+    new_post.set('title', title)
+    new_post.set('content', content)
+    new_post.save()
+    post_id = new_post.id
+    return post_id
