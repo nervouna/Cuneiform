b3d76067ec88998eba8aff02bf1ab5edb218e62e
Guan Xiaoyu <guanxy@me.com>
2016-11-30 02:30:28 +0800

    feat(templates): form a basic template structure

--- app.py
+++ app.py
@@ -32,7 +32,11 @@ def index(post_per_page=10):
     more = has_more_posts(current_page, post_count, post_per_page)
     return render_template('index.html', posts=posts, more=more, page=current_page)
 
-@app.route('/', methods=['POST'])
+@app.route('/new_post')
+def new_post_editor():
+    return render_template('editor.html')
+
+@app.route('/new_post', methods=['POST'])
 def new_post():
     post_title, post_content = request.form['title'], request.form['content']
     create_new_post(title=post_title, content=post_content)
