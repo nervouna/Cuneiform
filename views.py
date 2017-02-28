04f5b372848fb9b46199445385ab46e2e497bd25
Guan Xiaoyu <guanxy@me.com>
2017-03-01 01:20:31 +0800

    feat: check filename before save

--- views.py
+++ views.py
@@ -73,7 +73,7 @@ def create_post():
 	post.set(post_data)
 
 	upload_image = request.files['featured_image']
-	if upload_image.filename != '':
+	if upload_image.filename != '' and allowed_file(upload_image.filename):
 		try:
 			f = Attachment(upload_image.filename, data=upload_image.stream)
 		except TypeError as e:
