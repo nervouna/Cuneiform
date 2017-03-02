a6264253425f1791c642910d5144d53e22341af2
Guan Xiaoyu <guanxy@me.com>
2017-03-02 14:18:04 +0800

    feat: add helper funcs for tags

--- helpers.py
+++ helpers.py
@@ -1,7 +1,8 @@
 import re
 from functools import wraps
 from flask import abort
-from models import Author
+from leancloud import LeanCloudError
+from models import Author, Tag, TagPostMap
 
 
 def protected(func):
@@ -15,14 +16,6 @@ def protected(func):
         else:
             return func(*args, **kwargs)
     return secret_view
-
-
-def parse_tag_names(tag_string):
-    tag_string = tag_string.strip()
-    if len(tag_string) == 0:
-        return None
-    else:
-        return set(x.strip() for x in re.split('[,，;；、]', tag_string) if len(x) > 0)
 
 
 def allowed_file(ext):
@@ -39,3 +32,44 @@ def validate_form_data(raw_data):
         if field not in form_data.keys():
             raise ValueError
     return form_data
+
+
+def parse_tag_names(tag_string):
+    tag_string = tag_string.strip()
+    if len(tag_string) == 0:
+        return None
+    else:
+        return set(x.strip() for x in re.split('[,，;；、]', tag_string) if len(x) > 0)
+
+
+def get_tag_by_name(tag_name):
+    query = Tag.query.equal_to('name', tag_name)
+    try:
+        tag = query.first()
+    except LeanCloudError as e:
+        if e.code == 101:
+            tag = None
+        else:
+            raise e
+    return tag
+
+
+def create_tag_by_name(tag_name):
+    tag = Tag()
+    tag.set('name', tag_name)
+    tag.save()
+    return tag
+
+
+def map_tag_to_post(tags, post):
+    for tag in tags:
+        tag_post_map = TagPostMap()
+        tag_post_map.set('tag', tag)
+        tag_post_map.set('post', post)
+        try:
+            tag_post_map.save()
+        except LeanCloudError as e:
+            if e.code == 137:
+                pass
+            else:
+                raise e
