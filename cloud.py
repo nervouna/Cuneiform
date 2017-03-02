0624e30daea685c5c06bd8f5e85490d51a8c3ed9
Guan Xiaoyu <guanxy@me.com>
2017-03-02 17:28:10 +0800

    feat: delete 30-days-old trashed post via timer

--- cloud.py
+++ cloud.py
@@ -1,6 +1,8 @@
 from leancloud import Engine
 from markdown import markdown
+from datetime import datetime, timedelta, timezone
 
+from models import Post
 from app import app
 
 
@@ -12,5 +14,15 @@ def after_post_save(post):
     content = post.get('content')
     post.set('marked_content', markdown(content))
     post.save()
+
+
+@engine.define
+def delete_trashed_posts():
+    today = datetime.now(timezone(timedelta(hours=8)))
+    recycle_time = timedelta(30)
+    trashed_posts = Post.query.equal_to('trashed', True).find()
+    for post in trashed_posts:
+        if post.get('updatedAt') < today - recycle_time:
+            post.destroy()
 
 
