9850a31b739a8fdc67f765218317ea149b47a360
Guan Xiaoyu <guanxy@me.com>
2017-03-02 17:41:35 +0800

    feat: print timer log to console

--- cloud.py
+++ cloud.py
@@ -18,11 +18,18 @@ def after_post_save(post):
 
 @engine.define
 def delete_trashed_posts():
+    print('正在执行定时任务：删除回收站的帖子。')
     today = datetime.now(timezone(timedelta(hours=8)))
     recycle_time = timedelta(30)
     trashed_posts = Post.query.equal_to('trashed', True).find()
+    if len(trashed_posts) == 0:
+        print('回收站里没有帖子。')
+    else:
+        print('正在处理 %i 条帖子' % len(trashed_posts))
     for post in trashed_posts:
         if post.get('updatedAt') < today - recycle_time:
+            print(post.get('title'))
             post.destroy()
+            print('帖子已经被彻底删除。')
 
 
