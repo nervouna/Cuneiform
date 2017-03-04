e535276bba13577c7a1c063a303faae66b96b748
Guan Xiaoyu <guanxy@me.com>
2017-03-05 04:08:50 +0800

    feat: use after update hook to add updated flag

--- cloud.py
+++ cloud.py
@@ -6,3 +6,10 @@ from app import app
 
 
 engine = Engine(app)
+
+
+@engine.after_update('Post')
+def after_post_update(post):
+    post.set('is_updated', True)
+    post.save()
+    print('帖子已更新:', post.get('title'))
