7bc4142d417b75ccaaf5eca636005a18526bd1e8
Guan Xiaoyu <guanxy@me.com>
2017-03-07 23:24:21 +0800

    :construction: helpers

--- helpers.py
+++ helpers.py
@@ -2,8 +2,9 @@ import re
 from functools import wraps
 from flask import abort
 from markdown import markdown as m
+from leancloud import LeanCloudError
 
-from models import Author
+from models import Author, Tag, TagPostMap
 
 
 def protected(func):
@@ -27,3 +28,43 @@ def allowed_file(filename):
 
 def markdown(text):
     return m(text, extensions=['fenced_code'])
+
+
+def split_tag_names(tag_name_string):
+    splitters = ','
+    tag_names = set([x.strip() for x in re.split(splitters, tag_name_string)])
+    try:
+        tag_names.remove('')
+    except KeyError:
+        pass
+    return tag_names
+
+
+def get_tag_names_from_map_list(tag_post_maps):
+    tag_names = [x.get('tag').get('name') for x in tag_post_maps]
+    return tag_names
+
+
+def get_tag_by_name(tag_name):
+    try:
+        tag = Tag.query.equal_to('name', tag_name).first()
+    except LeanCloudError as e:
+        if e.code == 101:
+            tag = None
+        else:
+            raise e
+    return tag
+
+
+def set_tag_by_name(tag_name):
+    tag = Tag()
+    tag.set('name', tag_name)
+    tag.save()
+    return tag
+
+
+def map_tag_to_post(tags, post):
+    for tag in tags:
+        tag_post_map = TagPostMap()
+        tag_post_map.set({'tag': tag, 'post': post})
+        tag_post_map.save()
