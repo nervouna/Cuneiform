4fec709223501544b2e2b90eb0d7f48860409840
Guan Xiaoyu <guanxy@me.com>
2017-03-10 01:21:06 +0800

    :sparkles: purge trashed tag post maps

--- cloud.py
+++ cloud.py
@@ -1,10 +1,10 @@
 from datetime import datetime
 from datetime import timedelta
-from datetime import timezone
 
 from leancloud import Engine
 
 from models import Post
+from models import TagPostMap
 from app import app
 
 
@@ -16,3 +16,16 @@ def after_post_update(post):
     post.set('is_updated', True)
     post.save()
     print('帖子已更新:', post.get('title'))
+
+
+@engine.define
+def purge_trashed_tag_post_maps():
+	start_date = datetime.today() - timedelta(1)
+	trashed = TagPostMap.query.greater_than('updatedAt', start_date).equal_to('trashed', True).find()
+	if trashed:
+		print("正在清理 %i 个已删除的标签关系" % len(trashed))
+		for trashed_map in trashed:
+			trashed_map.destroy()
+		print("清理完毕")
+	else:
+		print("过去一天没有标签关系被删除")
